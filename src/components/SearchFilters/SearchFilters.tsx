e5fc7065fe48a16000bb11d6e4680df526239bd0
Nikk7 <n.sliyusar@gmail.com>
2023-12-22 15:34:31 +0200

    fix images issue

--- src/components/SearchFilters/SearchFilters.tsx
+++ src/components/SearchFilters/SearchFilters.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-
+// import arrowDown from '../../assets/images/arrowDown.svg';
+// import { ReactComponent as CustomComponentName } from '../../assets/images/arrowDown.svg';
+// import '../../assets/images/';
+// import arrowDown from '../../assets/images/arrowDown.svg';
+import arrowDown from '../../assets/images/arrowDown.svg';
 import "./SearchFilters.scss";
 
 const SearchFilters = () => {
 
-    const arrowDown = require('../../assets/images/arrowDown.svg') as string;
+    // const arrowDown = require('../../assets/images/arrowDown.svg') as string;
  
     return (
         <div className="container">
@@ -14,6 +18,7 @@ const SearchFilters = () => {
                 </div>
                 <div className="filter">
                
+                    {/* <img src={arrowDown} alt="" /> */}
                     <img src={arrowDown} alt="" />
                 </div>
             </div>
---
+++ src/components/Thumb/Thumb.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Thumb() {
+  return (
+    <div>Thumb</div>
+  )
+}
